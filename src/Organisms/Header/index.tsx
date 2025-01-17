7a06d93ad49a3d92d93f2246f1b6222827e6957a
artem <artemk2504@gmail.com>
2025-01-17 22:30:01 +0200

    feat:init header componenet

---
+++ src/Organisms/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Image from "next/image";
+export default function Header() {
+  return (
+    <header>
+      <Image src="" alt="" className="" />
+    </header>
+  );
+}
