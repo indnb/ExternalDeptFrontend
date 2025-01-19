84bb1f4a443764a26f1da3a2cd88834504d87a4c
artem <artemk2504@gmail.com>
2025-01-19 18:33:44 +0200

    feat:end title page

--- src/Molecules/MainLeftPanel/index.tsx
+++ src/Molecules/MainLeftPanel/index.tsx
@@ -6,14 +6,23 @@ export const MainLeftPanel = () => {
   const { language, changeLanguage } = useLanguageStore();
 
   return (
-    <div>
+    <div className="h-[738px] flex w-[52vw] flex-col justify-center items-center max-[650px]:h-[auto]">
+      <p
+        className="text-left text-[20px] text-[black] ml-[0px] w-[48vw] max-[650px]:text-[0px]"
+        style={{ fontFamily: "Montserrat" }}
+      >
+        {language == "ua" ? "Відділ зовнішніх зe’язків" : "Eng text"}
+      </p>
+
       <h1
-        className="text-[64px] w-[195px] max-[650px]:text-[40px] text-black leading-[40px] max-[650px]:mt-[22px] max-[650px]:w-[398px]"
+        className="h-[auto] text-[64px] w-[48vw] h-[auto] leading-[64px]  max-[650px]:text-[40px] text-black max-[650px]:leading-[40px] max-[650px]:mt-[22px] max-[650px]:w-[398px]"
         style={{ fontFamily: "Kharkiv Tone" }}
       >
-        EDUCATION WITHOUT BORDERS
+        EDUCATION
+        <br /> WITHOUT
+        <br /> BORDERS
       </h1>
-      <div className="mt-[27px]">
+      <div className="mt-[27px] max-[650px]:w-[auto] w-[50vw]">
         <Button
           title={
             language == "ua"
--- src/Organisms/Main/index.tsx
+++ src/Organisms/Main/index.tsx
@@ -4,7 +4,7 @@ import { MainLeftPanel } from "@/Molecules/MainLeftPanel";
 export const Main = () => {
   return (
     <main className="flex-row-reverse flex items-center justify-between max-[650px]:justify-start bg-white max-[650px]:flex-col  max-[650px]:h-[580px]">
-      <div className="h-[738px] w-[746px] max-[650px]:w-[398px] max-[650px]:h-[290px] bg-[#203C8F] max-[650px]:mt-[30px] rounded-[8px]"></div>
+      <div className="h-[738px] w-[48vw] max-[650px]:w-[398px] max-[650px]:h-[290px] bg-[#203C8F] max-[650px]:mt-[30px] rounded-[8px]"></div>
       <MainLeftPanel />
     </main>
   );
