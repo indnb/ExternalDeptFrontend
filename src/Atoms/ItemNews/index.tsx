1524dba513f2296c907dbc5e073aa03a07d01721
artem <artemk2504@gmail.com>
2025-01-23 00:44:19 +0200

    feat:media fix

--- src/Atoms/ItemNews/index.tsx
+++ src/Atoms/ItemNews/index.tsx
@@ -6,9 +6,9 @@ export const ItemNews: React.FC<IItemNews> = ({ title }) => {
   return (
     <div
       style={{ display: "flex" }}
-      className="flex flex-col items-start justify-between max-[650px]:h-[225px] max-[650px]:w-[299px] ml-[16px] mr-[16px]"
+      className="h-[435px] flex flex-col items-start justify-between max-[850px]:h-[225px] max-[850px]:w-[299px] ml-[16px] mr-[16px]"
     >
-      <div className="bg-[#203C8F] max-[650px]:w-[299px] max-[650px]:h-[191px] rounded-[16px]"></div>
+      <div className=" w-[555px] h-[376px] bg-[#203C8F] max-[850px]:w-[299px] max-[*50px]:h-[191px] rounded-[16px]"></div>
       <p
         className="block  font-bold  text-black text-[16px] text-left mt-[8px]"
         style={{ fontFamily: "Montserrat" }}
--- src/Organisms/News/index.tsx
+++ src/Organisms/News/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { NewsSlider } from "@/Molecules/NewsSlider";
+import { useLanguageStore } from "@/_store/LanguageChanger";
 
 export const News = () => {
+  const { language, changeLanguage } = useLanguageStore();
+
   return (
-    <section className="max-[650px]:h-[290px] flex flex-col items-center justify-around relative">
+    <section className="max-[850px]:h-[290px] flex flex-col items-center justify-around relative">
       <h1
-        className="max-[650px]:w-[90vw] text-black text-[32px]  "
+        className="max-[850px]:w-[90vw] w-[95vw] text-black max-[850px]:text-[32px]  text-[48px]"
         style={{ fontFamily: "Kyiv Type Sans" }}
       >
-        News
+        {language == "ua" ? "Новини" : "News"}
       </h1>
       <NewsSlider />
     </section>
