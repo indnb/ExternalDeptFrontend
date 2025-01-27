5a696f707b22084043ce6be039b83d6a7cd9381c
Prokhor1337 <chertilin2006@gmail.com>
2025-01-27 17:43:24 +0200

    feat: animation

--- src/Atoms/HeaderImage/index.tsx
+++ src/Atoms/HeaderImage/index.tsx
@@ -1,11 +1,11 @@
 "use client";
-import Dyikt from "../../public/Dyiktwhite.png";
+import Dyikt from "../../public/logo.png";
 
 export const Headerimage = () => {
     return (
         <div>
             <img
-                className="max-w-none w-auto h-[50px] md:h-[111px] object-contain"
+                className="max-w-none w-auto h-[50px] md:h-[90px] object-contain"
                 src={Dyikt.src}
             />
         </div>
