120635c6dfb24a475b336ec117cecc0640b95209
artem <artemk2504@gmail.com>
2025-02-15 02:11:31 +0200

    feat category

--- src/Atoms/ItemCategoryHackathon/index.tsx
+++ src/Atoms/ItemCategoryHackathon/index.tsx
@@ -4,9 +4,15 @@ import React from 'react'
 export const ItemCategoryHackathon = ({ img, description }: { img: string, description: string }) => {
   return (
     <div className=' flex items-center justify-center w-[83vw] border-t-2 border-black'>
-      <div className='w-[63vw] max-[850px]:mb-[12px] max-[850px]:mt-[12px] flex justify-between flex-col items-start'>
-        <Image className=' max-[850px]:w-[auto] max-[850px]:h-[25px]' src={img} alt="img category" />
-        <h1 className='max-[850px]:mt-[11px] text-[10px] text-black'>
+      <div className='max-[850px]:w-[63vw] max-[850px]:mb-[12px] max-[1300px]:mt-[25px] max-[1300px]:mb-[25px] mb-[50px] mt-[50px] max-[850px]:mt-[12px] flex justify-between   items-center max-[850px]:flex-col items-start'>
+        <div className='max-[850px]:w-auto w-[41vw] flex items-start justify-start'>
+          <Image
+            className="w-auto  max-[850px]:h-[25px] max-[1300px]:h-[50px] h-[78px] object-contain"
+            src={img}
+            alt="img category"
+          />
+        </div>
+        <h1 className='max-[850px]:mt-[11px] max-[850px]:w-[auto] w-[39vw]  text-[calc(23/1552*100vw)] max-[550px]:text-[10px] text-black'>
           {description}
         </h1>
       </div>
--- src/Atoms/RegisterButton/index.tsx
+++ src/Atoms/RegisterButton/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react'
 
 export interface IRegisterButton {
@@ -7,9 +8,9 @@ export interface IRegisterButton {
 }
 export const RegisterButton: React.FC<IRegisterButton> = ({ img, text, callback }) => {
   return (
-    <button onClick={callback}>
-      <img src={img} alt="button icon" />
-      <span>{text}</span>
+    <button className="max-[850px]:w-[191px] max-[850px]:h-[150px]   " onClick={callback}>
+      <Image src={img} alt="button icon" />
+      <span className='text-[calc(23/1552*100vw) max-[850px]:text-[12px] text-black'>{text}</span>
     </button>
   );
 };
