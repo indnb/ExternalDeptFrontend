0aa8b3735af761bf6f2ddc5d0926e6acb77bf02c
artem <artemk2504@gmail.com>
2025-02-20 18:46:44 +0200

    bugfix:media hackathon info

--- src/Molecules/HackathonInfoPanelBottom/index.tsx
+++ src/Molecules/HackathonInfoPanelBottom/index.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 export const HackathonInfoPanelBottom = ({ firstRow, secondRow }: { firstRow: string, secondRow: string }) => {
   return (
-    <div className='w-[100vw] flex justify-center mt-[-50px] max-[850px]:mt-[-10px]'>
+    <div className='w-[100vw] flex justify-center mt-[-50px] max-[850px]:mt-[-0px] max-w-[1800px]'>
       <div className='w-[47vw]     text-center'>
-        <h1 style={{ fontFamily: "Montserrat" }} className=' h-full text-left text-[calc(31/1552*100vw)]  max-[550px]:text-[10px]
+        <h1 style={{ fontFamily: "Montserrat" }} className=' h-full text-left text-[calc(31/1552*100vw)] min-[1800px]:text-[36px]  max-[550px]:text-[10px]
  text-[#000000] flex items-center justify-center '>
           {firstRow}
         </h1>
 
       </div>
-      <div style={{ fontFamily: "Kyiv Type Sans" }} className='w-[47vw]   max-[850px]:h-full text-[#203C8F] text-[calc(256/1552*100vw)] flex items-center justify-center'>
+      <div style={{ fontFamily: "Kyiv Type Sans" }} className='w-[47vw]   max-[850px]:h-full text-[#203C8F] text-[calc(256/1552*100vw)] min-[1800px]:text-[300px] 
+        flex items-center justify-center'>
         {secondRow}
       </div>
     </div>
--- src/Molecules/HackathonInfoPanelTop/index.tsx
+++ src/Molecules/HackathonInfoPanelTop/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const HackathonInfoPanelTop = ({ firstRow, secondRow }: { firstRow: string, secondRow: string }) => {
   return (
-    <div className='w-[100vw] flex justify-center '>
-      <div className='w-[48vw] h-full  text-[#203C8F] text-left'>
-        <h1 className='h-full  flex items-center  text-[calc(250/1552*100vw)]' style={{ fontFamily: "Kyiv Type Sans" }}
+    <div className='w-[100vw] flex justify-center max-w-[1800px]'>
+      <div className='w-[47vw] h-full  text-[#203C8F] text-left'>
+        <h1 className='h-full  flex items-center min-[1800px]:text-[300px] , text-[calc(250/1552*100vw)]' style={{ fontFamily: "Kyiv Type Sans" }}
 
         >
           {firstRow}
@@ -12,7 +12,7 @@ export const HackathonInfoPanelTop = ({ firstRow, secondRow }: { firstRow: strin
 
       </div>
       <div style={{ fontFamily: "Montserrat" }}
-        className='font-normal w-[48vw]  text-[#000000] text-[calc(32/1552*100vw)]  max-[550px]:text-[10px] flex items-center justify-center'>
+        className='font-normal w-[48vw] min-[1800px]:text-[34px]  text-[#000000] text-[calc(32/1552*100vw)]  max-[550px]:text-[10px] flex items-center justify-center'>
         {secondRow}
       </div>
     </div>
